a471ad95278a19d6ebf5919bee9bc496789930a3
oscarrobertson <or13827@my.bristol.ac.uk>
2015-06-23 15:38:38 +0100

    typo

--- heightmaps/heightmaps_app/views.py
+++ heightmaps/heightmaps_app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'heightmaps/index.html')
+	return render(request, 'pages/index.html')
