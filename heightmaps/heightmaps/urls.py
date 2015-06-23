f803d1676eb594592acba6edcda7e9de5fad1716
oscarrobertson <or13827@my.bristol.ac.uk>
2015-06-23 14:53:49 +0100

    Initial index page

--- heightmaps/heightmaps/urls.py
+++ heightmaps/heightmaps/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^map/', include('heightmaps.urls')),
+    url(r'^$', include('heightmaps.urls')),
 ]
--- heightmaps/heightmaps_app/urls.py
+++ heightmaps/heightmaps_app/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^export/', views.exportMap, name='exportMap'),
     url(r'^$', views.index, name='index'),
 
 ]
